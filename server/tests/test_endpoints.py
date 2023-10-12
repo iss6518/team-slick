666f54d59ae63dd6f272bf63e42d419d7e5316d3
iss6518 <113126730+iss6518@users.noreply.github.com>
2023-10-12 16:56:44 -0400

    Improving test_hello
    Attempting to fix error here

--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -4,7 +4,7 @@ import server.endpoints as ep
 TEST_CLIENT = ep.app.test_client()
 
 def test_hello():
-    resp = TEST_CLIENT.get(ep.HELLO_EP)
+    resp = TEST_CLIENT.get(ep./hello)
     print(f'{resp=}')
     resp_json = resp.get_json()
     print(f'{resp_json=}')
