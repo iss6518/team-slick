ef410955468e4adea2c8a521863e1c1eed7dd6bf
Iccha02 <iccha02@gmail.com>
2023-12-13 16:57:43 -0500

    MongoDB Cloud DB Backup (assigment)

--- db/bkup.sh
+++ db/bkup.sh
@@ -1,4 +1,4 @@
-#!/bin.sh
+#!/bin/bash
 # Script to backup production db to JSON files.
 
 
@@ -6,10 +6,10 @@
 
 for collection in ${MatchesCollections[@]}; do 
     echo "Backing up $collection"
-    $EXP --collection=$collection --db=$DB --out=$BKUP_DIR/$collection.json $CONNECT_STR --username $USER --password $MONGODB_PASSWORD
+    $EXP --collection=$collection --db=$DB --out=$BKUP_DIR/$collection.json $CONNECT_STR --username $USER --password $MONGODB_PASS
 done
 
-git add $BKUP_DIR/*.json
+git add $BKUP_DIR/*.jsonc
 git add $BKUP_DIR/*.json
 git commit $BKUP_DIR/*.json -m "Mongo DB backup"
 git pull origin master
--- db/common.sh
+++ db/common.sh
@@ -5,16 +5,16 @@ echo "Importing from common.sh"
 
 DB=commongroundDB
 USER=iccha02
-CONNECT_STR="mongodb+srv://koukoumongo1.yuf9b.mongodb.net/"
+CONNECT_STR="mongodb+srv://atlascluster.xd0fj6a.mongodb.net/"
 if [ -z $DATA_DIR ]
-then 
-    DATA_DIR=~/team-slick/db
-fi 
+then
+    DATA_DIR=/Users/icchasingh/softwareEngClass/team-slick/db
+fi
 BKUP_DIR=$DATA_DIR/bkup
 EXP=/usr/local/bin/mongoexport
 IMP=/usr/local/bin/mongoimport
 
-if [ -z $MONGODB_PASSWORD ]
+if [ -z $MONGODB_PASS ]
 then
     echo "You must set MONGODB_PASSWORD in your env before running this script."
     exit 1
