38afedb3511a094d9bacc5e730aabf4eaa831264
ProspectInc <rm5572@nyu.edu>
2023-12-17 15:16:05 +0400

    Update test_interface.py
    test for sendFriendReq

--- db/tests/test_interface.py
+++ db/tests/test_interface.py
@@ -93,6 +93,19 @@ def test_match_users():
     wrld.del_user(new_name)
     wrld.del_user(new_name2)
 
+def test_unmatch_users():
+    #match test users
+    new_name1 = wrld._get_test_name()
+    ret = wrld.add_user(new_name1, 30, "Female", "Hiking")
+    new_name2 = wrld._get_test_name()
+    ret = wrld.add_user(new_name2, 30, "Female", "Hiking")
+    wrld.match_users(new_name1, new_name2)
+    #unmatching 
+    unmatching = wrld.unmatch_users(new_name1, new_name2)
+    assert not isinstance(unmatching, bool)
+    wrld.del_user(new_name1)
+    wrld.del_user(new_name2)
+
 def test_fetch_friendReqs():
     dictlen = len(wrld.fetch_friendReqs())
     assert dictlen >= 0
@@ -112,22 +125,21 @@ def test_deleteFriendReq():
     new_name = wrld._get_test_name()
     ret1 = wrld.add_user(new_name, 30, "Female", "hiking")
     friendreqs = wrld.fetch_friendReqs()
-    for user in friendreqs:
-        assert isinstance(user, str)
-        assert isinstance(friendreqs[user], dict)
-    wrld.del_user(new_name)
-    
-# def test_sendFriendReq():
-
-def test_unmatch_users():
-    #match test users
-    new_name1 = wrld._get_test_name()
-    ret = wrld.add_user(new_name1, 30, "Female", "Hiking")
     new_name2 = wrld._get_test_name()
-    ret = wrld.add_user(new_name2, 31, "Female", "Hiking")
-    wrld.match_users(new_name1, new_name2)
-    #unmatching 
-    unmatching = wrld.unmatch_users(new_name1, new_name2)
-    assert not isinstance(unmatching, bool)
-    wrld.del_user(new_name1)
+    ret = wrld.add_user(new_name2, 30, "Female", "Hiking")
+    success = deleteFriendReq(new_name, new_name2)
+    for user in friendreqs:
+        if (user == new_name):
+            assert isinstance(friendreqs[new_name], dict)
+    wrld.del_user(new_name)
+    wrld.del_user(new_name2)
+
+def test_sendFriendReq():
+    new_name = wrld._get_test_name()
+    ret = wrld.add_user(new_name, 30, "Female", "Hiking")
+    new_name2 = wrld._get_test_name()
+    ret = wrld.add_user(new_name2, 30, "Female", "Hiking")
+    success = sendFriendReq(new_name1, new_name2)
+    assert isinstance (success, bool)
+    wrld.del_user(new_name)
     wrld.del_user(new_name2)
